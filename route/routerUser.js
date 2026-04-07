e39a80d12fae5bc355e042396c842f2e8d7a3498
Eliam Araya <eliamaa02@gmail.com>
2026-04-06 22:00:45 -0600

    Se agrega la ruta de verify

--- route/routerUser.js
+++ route/routerUser.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { userPost, userGet } = require('../controller/controllerUser');
+const { userPost, userGet, userVerify } = require('../controller/controllerUser');
 
 router.post('/users', userPost);
 router.get('/users', userGet);
+router.get('/users/verify', userVerify);
+
 module.exports = router;
