31b23ed94263679e97e03331f3c405076fd35bcb
Eliam Araya <eliamaa02@gmail.com>
2026-04-09 15:27:14 -0600

    Agregar ruta google/cedula

--- route/routerUser.js
+++ route/routerUser.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { userPost, userGet, userVerify } = require('../controller/controllerUser');
+const { userPost, userGet, userVerify, googleCedula } = require('../controller/controllerUser');
+const { authenticateToken } = require('../controller/controllerAuth');
 
 router.post('/users', userPost);
 router.get('/users', userGet);
 router.get('/users/verify', userVerify);
-
+router.post('/users/google/cedula', authenticateToken, googleCedula);
 module.exports = router;
